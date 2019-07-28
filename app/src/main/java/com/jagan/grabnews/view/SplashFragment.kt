c96908667417638a96871f3917839410569dc6a0
Jaganathan <jaganathan.g@caratlane.com>
2019-07-28 16:52:28 +0530

    added navigation component

---
+++ app/src/main/java/com/jagan/grabnews/view/SplashFragment.kt
@@ -0,0 +1,45 @@
+package com.jagan.grabnews.view
+
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+import com.jagan.grabnews.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class SplashFragment : Fragment() {
+
+    var navController :NavController? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        Handler().postDelayed({
+            navController!!.navigate(R.id.action_splashFragment_to_newsFragment)
+        }, 2000)
+    }
+
+
+}
